a0a4a6120665c71cce804fa50fe8a14f7d3dee39
bhaskarkesavulu <bhaskar120689@gmail.com>
2024-03-16 12:03:33 +0530

    removed console

--- ems-service/src/services/EmployeeService.js
+++ ems-service/src/services/EmployeeService.js
@@ -16,4 +16,3 @@ export const createmployee = (employee) => axios.post(REST_API_POST_BASE_URL, em
 export const getEmployee = (employeeId) => axios.get(REST_API_GET_BASE_URL + '/' + employeeId);
 export const updateEmployee = (employeeId, employee) => axios.put(REST_API_PUT_BASE_URL + '/' + employeeId, employee);
 export const deleteEmployee = (employeeId) => axios.delete(REST_API_DELETE_BASE_URL + '/' + employeeId);
-console.log(employeeId);
